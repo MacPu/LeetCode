7f961a7df4b3bcd0277781376c7f4f913b0a4adc
MacPu <609779377@qq.com>
2015-07-12 00:46:31 +0800

    init project and solute first problem

---
+++ Python/1_TwoSum.py
@@ -0,0 +1,14 @@
+__author__ = 'MacPu'
+
+class Solution:
+    def twoSum(self, nums, target):
+        lookup = {}
+        for i, num in enumerate(nums):
+            if target - num in lookup:
+                return (lookup[target - num] + 1, i + 1)
+            lookup[num] = i
+
+if __name__ == '__main__':
+    print "index1=%d, index2=%d" % Solution().twoSum((2, 7, 11, 15), 9)
+
+
